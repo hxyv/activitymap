1f940b5c8dc2719c0c97cf25226bd783dfaf9351
Xingyu <xingyu3906@gmail.com>
2022-11-28 11:23:23 +0800

    Unify filename capitalization: add

---
+++ R/activitymap.R
@@ -0,0 +1,11 @@
+#' activitymap: A package using Garmin fit files to plot your activity map.
+#'
+#' Provides functionality for reading fit files and ploting activity map.
+#'
+#' @docType package
+#' @name activitymap
+#'
+#' @import FITfileR
+#'
+#'
+NULL
